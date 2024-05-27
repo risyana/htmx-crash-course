c2f31de44d5539c504d2186ebdaba61836f12839
Eka Risyana <eka.risyana@mekari.com>
2024-05-27 20:27:28 +0700

    use hx-get and hx-swap

--- server.js
+++ server.js
@@ -8,6 +8,22 @@ app.use(express.urlencoded({extended: true}))
 
 app.use(express.json())
 
+app.get('/users', (req, res) => {
+    const users = [
+        {id: 1, name: 'Hei'},
+        {id: 2, name: 'Don'},
+        {id: 3, name: 'Dave'},
+        {id: 4, name: 'Darren'},
+        {id: 5, name: 'Dody'},
+    ]
+
+    res.send(`
+    <h1 class="text-2xl font-bold my-4"> Users </h1>
+    <ul>
+    ${users.map(user => `<li>${user.name}</li>`).join('') }
+    </ul>`)
+})
+
 app.listen(3000, () => {
     console.log('server listeting to port 3000')
 })
