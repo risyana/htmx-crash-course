cc2cf70d47ea9633b38f8d81dda3482d9f399d9a
Eka Risyana <eka.risyana@mekari.com>
2024-05-27 20:48:26 +0700

    use hx-indicator to show loading

--- server.js
+++ server.js
@@ -10,14 +10,17 @@ app.use(express.json())
 
 app.get('/users', async (req, res) => {
 
-    const response = await fetch('https://jsonplaceholder.typicode.com/users')
-    const users = await response.json()
+    setTimeout(async () => {
+        const response = await fetch('https://jsonplaceholder.typicode.com/users')
+        const users = await response.json()
 
-    res.send(`
-    <h1 class="text-2xl font-bold my-4"> Users </h1>
-    <ul>
-    ${users.map(user => `<li>${user.name}</li>`).join('') }
-    </ul>`)
+        res.send(`
+        <h1 class="text-2xl font-bold my-4"> Users </h1>
+        <ul>
+        ${users.map(user => `<li>${user.name}</li>`).join('') }
+        </ul>`)
+        
+    }, 2000);
 })
 
 app.listen(3000, () => {
